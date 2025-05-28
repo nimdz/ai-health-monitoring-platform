bec8872d9404edcf2faf9d5bd357914da3b4ea89
Nimsara De Zoysa <105286867+nimdz@users.noreply.github.com>
2025-05-28 21:05:54 +0530

    add role based access

---
+++ user-service/src/middleware/roleMiddleware.js
@@ -0,0 +1,16 @@
+const authorize = (roles = []) => {
+  if (typeof roles === "string") {
+    roles = [roles];
+  }
+
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res
+        .status(403)
+        .json({ error: "Forbidden: insufficient permissions" });
+    }
+    next();
+  };
+};
+
+module.exports = authorize;
